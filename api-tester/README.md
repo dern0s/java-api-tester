a80c3e3b4bfa56960a84c3cab24cb1eecbbe5503
Denis Alves <denis.alves.hu@gmail.com>
2023-08-03 17:34:57 -0300

    created project

---
+++ api-tester/README.md
@@ -0,0 +1,5 @@
+# Well, hello there!
+
+This is just a simple example, extracted from Spring's documentantion to check if things are working.
+
+It worked on my pc.
---
+++ api-tester/src/test/java/com/apitest/apitester/ApiTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apitest.apitester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
